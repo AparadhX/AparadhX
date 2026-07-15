9e35fa50807277627a035b43f7ab87625765d852
AparadhX <aligopalganj66@gmail.com>
2026-07-15 12:50:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-// AparadhX Welcome Animation
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
 
-window.onload = function(){
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("show");
+});const topBtn = document.getElementById("topBtn");
 
-    console.log("Welcome to AparadhX");
-
-};
-
-
-// Button Click Effect
-
-const button = document.querySelector("button");
-
-button.addEventListener("click", function(){
-
-    alert("Latest Crime Episodes Coming Soon!");
-
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
 });
 
-
-// Smooth Scroll Effect
-
-document.querySelectorAll("a").forEach(link => {
-
-    link.addEventListener("click", function(e){
-
-        e.preventDefault();
-
+topBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
+});const topBtn = document.getElementById("topBtn");
 
-});const search = document.getElementById("searchBox");
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
 
-search.addEventListener("keyup", function(){
-
-console.log("Searching:", search.value);
-
-});window.addEventListener("load",()=>{
-
-document.querySelector(".loader").style.display="none";
-
+topBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
